45484b5ac2b61c4b11a549d0dd7c2b9590f65532
YANAGISAWA, COLE KAZUYUKI <237541554+kazuyanagisawa@users.noreply.github.com>
2026-06-04 19:11:14 -0400

    Initial Commit

---
+++ TestStandDAQ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestStandDAQ
+//
+//  Created by Kazu on 6/4/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
