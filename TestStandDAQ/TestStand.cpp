a5bbafc372416aee710263081bd450d24c0ef067
YANAGISAWA, COLE KAZUYUKI <237541554+kazuyanagisawa@users.noreply.github.com>
2026-06-05 16:44:46 -0400

    Consolidate anomaly reporting into summary

--- TestStandDAQ/TestStand.cpp
+++ TestStandDAQ/TestStand.cpp
@@ -97,10 +97,6 @@ void TestStand::printSummary() const
 
     TestSummary summary = generateTestSummary(readings);
     printTestSummary(summary);
-
-    cout << "Anomalies detected: "
-         << countAnomalies()
-         << endl;
 }
 
 void TestStand::printSensorStatisticsReport() const
